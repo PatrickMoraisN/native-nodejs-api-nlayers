cef65e203ddef63f8ae6695812f5f38d261090e7
Patrick Morais <ppternunes@gmail.com>
2022-05-17 17:21:49 -0300

    feat: create heroFactory

---
+++ src/factories/heroFactory.js
@@ -0,0 +1,12 @@
+import HeroRepository from "../repositories/heroRepository.js"
+import HeroService from "../services/heroService.js"
+
+export const generateInstance = ({ filePath}) => {
+  // here goes all db connections...
+
+  const heroRepository = new HeroRepository({ file: filePath });
+
+  const heroService = new HeroService({ heroRepository });
+
+  return heroService;
+}
